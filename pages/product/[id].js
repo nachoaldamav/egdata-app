272d36fe2230f9128771ad51c507ad69bc2d0d66
srdrabx <devalladolidoficial@gmail.com>
2020-12-26 00:29:50 +0100

    Final stage of individual games

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -5,26 +5,32 @@ import Link from "next/link"
 import { useRouter } from 'next/router'
 import useSWR from 'swr'
 
-export default function Product(context) {
-    const { data, error } = useSWR('https://api.trackstats.app/game.php', fetch)
+export default function Game({ id }) {
 
-    if (error) return (
-        <AppLayout><p className="text-5xl text-white text-left py-5">Failed to load</p></AppLayout>
-    )
-    if (!data) return (
+    const { game, isLoading, isError } = useGame(id)
+    if (isLoading) return (
         <AppLayout><p className="text-5xl text-white text-left py-5">Loading...</p></AppLayout>
     )
+    if (isError) return (
+        <AppLayout><p className="text-5xl text-white text-left py-5">Failed to load</p></AppLayout>
+    )
 
+    return (
+        <AppLayout>
+            <p className="text-5xl text-white text-left py-5">{game.title}</p>
+        </AppLayout>
+    )
+}
+
+function useGame (context) {
     const router = useRouter()
     const { id } = router.query
 
-        if (router.isFallback) return (
-            <AppLayout><p className="text-5xl text-white text-left py-5">Loading...</p></AppLayout>
-        )
-
-        return (
-            <AppLayout>
-                <p className="text-white text-xl">{data.title}</p>
-            </AppLayout>
-        )
-}
+    const fetcher = (...args) => fetch(...args).then(res => res.json())
+    const { data, error } = useSWR(`https://api.trackstats.app/game.php?title=${id}`, fetcher)
+    return {
+      game: data,
+      isLoading: !error && !data,
+      isError: error
+    }
+  }
