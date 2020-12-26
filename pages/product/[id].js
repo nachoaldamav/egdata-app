95a56957c0bf1adeda4437a4145614bc5518e487
srdrabx <devalladolidoficial@gmail.com>
2020-12-26 01:27:29 +0100

    added skeleton while loading

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -4,12 +4,26 @@ import AppLayout from '../../components/AppLayout'
 import Link from "next/link"
 import { useRouter } from 'next/router'
 import useSWR from 'swr'
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
 
 export default function Game({ id }) {
 
     const { game, isLoading, isError } = useGame(id)
     if (isLoading) return (
-        <AppLayout><p className="text-5xl text-white text-left py-5">Loading...</p></AppLayout>
+        <AppLayout>
+            <div className="container mx-auto px-6 py-2">
+                <SkeletonTheme color="#202020" highlightColor="#444">
+                    <p>
+                        <Skeleton height={630} width={1100} duration={1.2} />
+                    </p>
+                </SkeletonTheme>
+                <SkeletonTheme color="#202020" highlightColor="#444">
+                    <p>
+                        <Skeleton height={50} width={300} duration={1.2} />
+                    </p>
+                </SkeletonTheme>
+            </div>
+        </AppLayout>
     )
     if (isError) return (
         <AppLayout><p className="text-5xl text-white text-left py-5">Failed to load</p></AppLayout>
@@ -20,7 +34,15 @@ export default function Game({ id }) {
             <Head>
                 <title>{game.title} | Epic Games Data</title>
             </Head>
-            <p className="text-5xl text-white text-left py-5">{game.title}</p>
+            <div className="container mx-auto px-6 py-2 place-content-center">
+                <Image
+                 src={game.DieselStoreFrontWide || game.OfferImageWide || <Skeleton />}
+                 width={1100}
+                 height={630}
+                 className="object-center place-content-center"
+                 />
+                <p className="text-5xl text-white text-left py-5">{game.title || <Skeleton />}</p>
+            </div>
         </AppLayout>
     )
 }
