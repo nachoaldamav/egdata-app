473ff68a83d3bf13ae11d7445fdd15679115a1ff
srdrabx <devalladolidoficial@gmail.com>
2020-12-26 21:33:05 +0100

    Added loading bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import '../styles/globals.css'
 import '../styles/tailwind.css'
-import { Navbar } from '../components/navbar'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+import Router  from 'next/router'
+
+//Binding events.
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps }) {
   return (
