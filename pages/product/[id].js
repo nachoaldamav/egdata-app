891a2d6989f3a7ef983bdff4d9049797dbc2c39e
srdrabx <devalladolidoficial@gmail.com>
2020-12-26 12:01:02 +0100

    Direct buy support

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -4,7 +4,7 @@ import AppLayout from '../../components/AppLayout'
 import Link from "next/link"
 import { useRouter } from 'next/router'
 import useSWR from 'swr'
-import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
+import Skeleton from '@material-ui/lab/Skeleton';
 
 export default function Game({ id }) {
 
@@ -12,16 +12,9 @@ export default function Game({ id }) {
     if (isLoading) return (
         <AppLayout>
             <div className="container mx-auto px-6 py-2">
-                <SkeletonTheme color="#202020" highlightColor="#444">
-                    <p>
-                        <Skeleton height={630} width={1100} duration={1.2} />
-                    </p>
-                </SkeletonTheme>
-                <SkeletonTheme color="#202020" highlightColor="#444">
-                    <p>
-                        <Skeleton height={50} width={300} duration={1.2} />
-                    </p>
-                </SkeletonTheme>
+                <Skeleton variant="rect" width="75%" height={630} animation="wave" />
+                <br />
+                <Skeleton variant="text" width="25%" animation="wave" />
             </div>
         </AppLayout>
     )
@@ -34,7 +27,7 @@ export default function Game({ id }) {
             <Head>
                 <title>{game.title} | Epic Games Data</title>
             </Head>
-            <div className="container mx-auto px-6 py-2 place-content-center">
+            <div className="container mx-auto px-6 py-2 content-center items-center">
                 <Image
                  src={game.DieselStoreFrontWide || game.OfferImageWide || <Skeleton />}
                  width={1100}
@@ -42,6 +35,20 @@ export default function Game({ id }) {
                  className="object-center place-content-center"
                  />
                 <p className="text-5xl text-white text-left py-5">{game.title || <Skeleton />}</p>
+                <div className="inline-flex rounded-md shadow">
+                    <Link href={game.productSlug}>
+                        <a target="_blank" className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-transparent hover:border-indigo-700">
+                            Buy now
+                        </a>
+                    </Link>
+                </div>
+                <div className="inline-flex px-2 rounded-md shadow">
+                    <Link href={`https://www.epicgames.com/store/purchase?namespace=${game.namespace}&showNavigation=true&highlightColor=0078f2&offers=${game._id}#/purchase/verify?_k=mshm8g`}>
+                        <a target="_blank" className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-transparent hover:border-indigo-700">
+                            Direct Buy
+                        </a>
+                    </Link>
+                </div>
             </div>
         </AppLayout>
     )
