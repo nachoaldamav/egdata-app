e30f8052d80d0f758cf8cfea74714b8d619ed404
srdrabx <devalladolidoficial@gmail.com>
2021-01-03 00:54:32 +0100

    Duplicated notification fix

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -19,22 +19,3 @@
  // messages.
  const messaging = firebase.messaging();
 
-
-
-// If you would like to customize notifications that are received in the
-// background (Web app is closed or not in browser focus) then you should
-// implement this optional method.
-// [START on_background_message]
-messaging.onBackgroundMessage(function(payload) {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
-  };
-
-  self.registration.showNotification(notificationTitle,
-    notificationOptions);
-});
-// [END on_background_message]
