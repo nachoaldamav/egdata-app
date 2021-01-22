d291c562c32850583ada55bfaa25edfee0ddbedf
srdrabx <devalladolidoficial@gmail.com>
2021-01-22 12:31:04 +0100

    Update 2

--- pages/login/callback/index.js
+++ pages/login/callback/index.js
@@ -4,18 +4,18 @@ import axios from 'axios'
 
 export default function loginCallback() {
     const router = useRouter()
-    console.log(router.query.code)
+    let callbackCode = router.query.code
     const bodyFormData = {
         grant_type: 'authorization_code',
         deployement_id: 'c1472b0ba8e14f87871a304e98b735ef',
         scope: 'basic_profile',
-        code: '11a7fbe4266842ffb386919cd645e781',
+        code: callbackCode,
         redirect_uri: 'https://egdata.app/'
     }
     axios({
         method: 'post',
         url: 'https://api.epicgames.dev/epic/oauth/v1/token',
-        data: JSON.stringify(bodyFormData),
+        data: bodyFormData,
         config: { headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
             'Authorization': 'Basic eHl6YTc4OTFsY29wWnJ0ZnR4WmJ3b1NZckx0UWlOTno6VXdsQSszQTJkS2FBcHNmUGNwWkhhSlZUMXNDb09HQ1VPWW1HbHFYaWRMbw=='
