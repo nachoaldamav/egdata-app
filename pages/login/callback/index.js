2555d9a6d7d2265c42d957e2d832bdff548f3ef8
srdrabx <devalladolidoficial@gmail.com>
2021-01-22 12:48:55 +0100

    Update 4

--- pages/login/callback/index.js
+++ pages/login/callback/index.js
@@ -5,24 +5,27 @@ import axios from 'axios'
 export default function loginCallback() {
     const router = useRouter()
     let callbackCode = router.query.code
-    const bodyFormData = {
-        grant_type: 'authorization_code',
-        deployement_id: 'c1472b0ba8e14f87871a304e98b735ef',
-        scope: 'basic_profile',
-        code: callbackCode,
-        redirect_uri: 'https://egdata.app/login/callback'
+
+    const params = new URLSearchParams()
+
+    params.append('grant_type', 'authorization_code')
+    params.append('deployment_id', 'c1472b0ba8e14f87871a304e98b735ef')
+    params.append('scope', 'basic_profile')
+    params.append('code', callbackCode)
+    params.append('redirect_uri', 'https://egdata.app/login/callback')
+    
+    const config = {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': 'Basic eHl6YTc4OTFsY29wWnJ0ZnR4WmJ3b1NZckx0UWlOTno6VXdsQSszQTJkS2FBcHNmUGNwWkhhSlZUMXNDb09HQ1VPWW1HbHFYaWRMbw=='
+        }
     }
-    axios({
-        method: 'post',
-        url: 'https://api.epicgames.dev/epic/oauth/v1/token',
-        data: bodyFormData,
-        config: { headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization': 'Basic eHl6YTc4OTFsY29wWnJ0ZnR4WmJ3b1NZckx0UWlOTno6VXdsQSszQTJkS2FBcHNmUGNwWkhhSlZUMXNDb09HQ1VPWW1HbHFYaWRMbw=='
-          }}
-    })
+
+    const url = 'https://api.epicgames.dev/epic/oauth/v1/token'
+
+    axios.post(url, params, config)
     .then((response) => {console.log(response)}) 
-    .catch(error => {console.log( 'the error has occured: ' + error), console.log(bodyFormData) })
+    .catch(error => {console.log( 'the error has occured: ' + error)})
 
     return (
         <AppLayout>
