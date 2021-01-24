750fa956927aa9b9036c11ebc337988935f83aed
srdrabx <devalladolidoficial@gmail.com>
2021-01-24 13:03:53 +0100

    Login with Epic

--- pages/login/index.js
+++ pages/login/index.js
@@ -5,14 +5,14 @@ export default function Login() {
   const [session, loading] = useSession()
   return (
     <AppLayout>
-      {!session && <>
-      Not signed in <br/>
-      <button onClick={() => signIn()}>Sign in</button>
-    </>}
-    {session && <>
+      {!session && <div className="text-white text-xl text-center">
+      <br/>
+      <button className="btn h-15 w-auto px-4 py-2 bg-black epic-login inline rounded-md" onClick={() => signIn()}><img className="inline m-1" height="25px" width="24" src="/img/Epic-Games-White-Solid.png" />Sign in with Epic Games</button>
+    </div>}
+    {session && <div className="text-white text-xl">
       Signed in as {session.user.name}<br/>
       <button onClick={signOut}>Sign out</button>
-    </>}
+    </div>}
     </AppLayout>
   )
 }
