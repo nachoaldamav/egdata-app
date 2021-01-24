84fb66286f77a97a36676a4eedafd425635a1e7a
srdrabx <devalladolidoficial@gmail.com>
2021-01-24 16:27:13 +0100

    Update

--- pages/login/index.js
+++ pages/login/index.js
@@ -5,10 +5,6 @@ export default function Login() {
   const [session, loading] = useSession()
   return (
     <AppLayout>
-      {loading && <div className="text-white text-xl text-center">
-        <p>Loading...</p>
-      </div>
-      }
       {!session && <div className="text-white text-xl text-center">
       <br/>
       <button className="btn h-15 w-auto px-4 py-2 bg-black epic-login inline rounded-md" onClick={() => signIn()}><img className="inline m-1" height="25px" width="24" src="/img/Epic-Games-White-Solid.png" />Sign in with Epic Games</button>
