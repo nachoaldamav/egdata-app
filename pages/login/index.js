c5d6269444c9689895f9278890a8f77d6f477f21
srdrabx <devalladolidoficial@gmail.com>
2021-01-24 16:21:57 +0100

    Update

--- pages/login/index.js
+++ pages/login/index.js
@@ -9,7 +9,7 @@ export default function Login() {
         <p>Loading...</p>
       </div>
       }
-      {!session, !loading && <div className="text-white text-xl text-center">
+      {!session && <div className="text-white text-xl text-center">
       <br/>
       <button className="btn h-15 w-auto px-4 py-2 bg-black epic-login inline rounded-md" onClick={() => signIn()}><img className="inline m-1" height="25px" width="24" src="/img/Epic-Games-White-Solid.png" />Sign in with Epic Games</button>
     </div>}
