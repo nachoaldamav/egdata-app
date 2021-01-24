55df19dec5fb4c16a9ff94a1267e9ae080764a25
srdrabx <devalladolidoficial@gmail.com>
2021-01-24 16:16:14 +0100

    Loading fallback 2

--- pages/login/index.js
+++ pages/login/index.js
@@ -5,7 +5,7 @@ export default function Login() {
   const [session, loading] = useSession()
   return (
     <AppLayout>
-      {!loading && <>
+      {loading && <>
         <p>Loading...</p>
       </>
       }
