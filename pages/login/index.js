11ea7ebfb2e2367c4db968d44a23a9d9bd067319
srdrabx <devalladolidoficial@gmail.com>
2021-01-24 11:11:08 +0100

    Try Next Auth

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,9 +1,38 @@
-import AppLayout from "../../components/AppLayout";
+import AppLayout from "../../components/AppLayout"
+import { signin, signout, useSession } from 'next-auth/client'
 
 export default function Login() {
+  const [session, loading] = useSession()
   return (
     <AppLayout>
-      <a className="text-white" href="https://www.epicgames.com/id/authorize?client_id=xyza7891lcopZrtftxZbwoSYrLtQiNNz&response_type=code&scope=basic_profile&redirect_uri=https://egdata.app/login/callback">Iniciar Sesión</a>
+      {!session && (
+          <>
+            <span>Not signed in</span>
+            <a
+              href={`/api/auth/signin`}
+              onClick={(e) => {
+                e.preventDefault()
+                signin()
+              }}
+            >
+              <button>Sign in</button>
+            </a>
+          </>
+        )}
+        {session && (
+          <>
+            <span>Signed in as {session.user.name}</span>
+            <a
+              href={`/api/auth/signout`}
+              onClick={(e) => {
+                e.preventDefault()
+                signout()
+              }}
+            >
+              <button>Sign out</button>
+              </a>
+          </>
+        )}
     </AppLayout>
   )
 }
