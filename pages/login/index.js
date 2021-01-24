a034377d968b5efcf9bd38a6615c77dfe1cfe541
srdrabx <devalladolidoficial@gmail.com>
2021-01-24 16:18:29 +0100

    Loading fallback 3

--- pages/login/index.js
+++ pages/login/index.js
@@ -5,15 +5,15 @@ export default function Login() {
   const [session, loading] = useSession()
   return (
     <AppLayout>
-      {loading && <>
+      {loading && <div className="text-white text-xl text-center">
         <p>Loading...</p>
-      </>
+      </div>
       }
-      {!session && <div className="text-white text-xl text-center">
+      {!session, !loading && <div className="text-white text-xl text-center">
       <br/>
       <button className="btn h-15 w-auto px-4 py-2 bg-black epic-login inline rounded-md" onClick={() => signIn()}><img className="inline m-1" height="25px" width="24" src="/img/Epic-Games-White-Solid.png" />Sign in with Epic Games</button>
     </div>}
-    {session && <div className="text-white text-xl text-center">
+    {session, !loading && <div className="text-white text-xl text-center">
       Signed in as {session.user.name}<br/>
       <button className="btn h-15 w-auto px-4 py-2 bg-black epic-login inline rounded-md" onClick={signOut}>Sign out</button>
     </div>}
