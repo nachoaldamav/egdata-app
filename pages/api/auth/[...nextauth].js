1fd4bb7e973ce708b02c7ca6406805d7bd5a71bc
srdrabx <devalladolidoficial@gmail.com>
2021-01-27 16:26:43 +0100

    Update

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,7 +10,7 @@ const options = {
         domain: process.env.AUTH0_DOMAIN
       })
   ],
-  database: process.env.DATABASE_URL,
+  database: "mongodb://srdrabx:@Cascarilla6a@cluster0-shard-00-00.0hnhr.mongodb.net:27017,cluster0-shard-00-01.0hnhr.mongodb.net:27017,cluster0-shard-00-02.0hnhr.mongodb.net:27017/epicgames",
   // Additional options
   // secret: 'abcdef123456789' // Recommended (but auto-generated if not specified)
   debug: true, // Use this option to enable debug messages in the console
