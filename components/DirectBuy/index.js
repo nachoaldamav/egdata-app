de5276c206cc34297a9fa341c9ab9651919222ac
srdrabx <devalladolidoficial@gmail.com>
2021-03-26 12:35:31 +0100

    Update button

--- components/DirectBuy/index.js
+++ components/DirectBuy/index.js
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 export default function DirectBuy({ children, customAttributes, id }) {
     const image = customAttributes.find(images => images.type === 'productHome')
-    const {key, value} = image
+    const {type, namespace} = image
 
     return (
-        <Link href={`https://www.epicgames.com/store/purchase?namespace=${id}&showNavigation=true&highlightColor=0078f2&offers=${value}#/purchase/verify?_k=mshm8g`}>
+        <Link href={`https://www.epicgames.com/store/purchase?namespace=${id}&showNavigation=true&highlightColor=0078f2&offers=${namespace}#/purchase/verify?_k=mshm8g`}>
             <a target="_blank" className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-transparent hover:border-indigo-700">
                 {children}
             </a>
