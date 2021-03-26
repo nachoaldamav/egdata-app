6391f674977701163cebbd94ec2f5274a5828580
srdrabx <devalladolidoficial@gmail.com>
2021-03-26 12:39:42 +0100

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: [
-    './src/**/*.html',
-    './src/**/*.vue',
-    './src/**/*.jsx',
-  ],
+  purge: [],
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
