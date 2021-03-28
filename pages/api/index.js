1acc4f1afa10bb7f6b1fcfc474a8dd0daa990842
srdrabx <devalladolidoficial@gmail.com>
2021-03-28 10:30:40 +0200

    added API

---
+++ pages/api/index.js
@@ -0,0 +1,27 @@
+const app = require('express')()
+const { v4 } = require('uuid')
+const axios = require('axios')
+
+app.get('/api', (req, res) => {
+  res.setHeader('Content-Type', 'text/html')
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
+  res.end(`Hello!`)
+})
+
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params
+  res.end(`Item: ${slug}`)
+})
+
+app.get('/api/trending', (req, res) => {
+    const data = async () => {
+        try {
+          return await axios.get('https://egs-api.trackstats.workers.dev/')
+        } catch (error) {
+          console.error(error)
+        }
+      };
+    res.send(data)
+})
+
+module.exports = app
