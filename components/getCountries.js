617e56f06e90dd722a54f116ef136396a2892026
srdrabx <devalladolidoficial@gmail.com>
2021-06-24 19:18:33 +0200

    Added India to Price DB

--- components/getCountries.js
+++ components/getCountries.js
@@ -1,6 +1,6 @@
 export default function getCountries() {
   const countryStr =
-    "US,PL,RU,ID,CN,AD,AU,BE,CH,DK,FR,IE,IT,JP,NO,DE,ES,IL,MC,MX,PR,VA,BR,TR,CZ,LV,FI,NL,CA,KR,LT,PT,SE,AE,AT,CY,LU,MT,SA,SI,SK,BY,UA,EE"
+    "US,PL,RU,ID,CN,AD,AU,BE,CH,DK,FR,IE,IT,JP,NO,DE,ES,IL,MC,MX,IN,PR,VA,BR,TR,CZ,LV,FI,NL,CA,KR,LT,PT,SE,AE,AT,CY,LU,MT,SA,SI,SK,BY,UA,EE"
   const countries = countryStr.split(",")
   return countries
 }
