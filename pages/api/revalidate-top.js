ac74fbfc25bf21611a3594be6c17c28f27dad08b
srdrabx <devalladolidoficial@gmail.com>
2022-06-12 12:01:49 +0200

    Add revalidate API

---
+++ pages/api/revalidate-top.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+  // Check for secret to confirm this is a valid request
+  if (req.query.secret !== process.env.SECRET_REVALIDATE) {
+    return res.status(401).json({ message: "Invalid token" })
+  }
+
+  try {
+    await res.unstable_revalidate("/top-sellers")
+    return res.json({ revalidated: true })
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send("Error revalidating")
+  }
+}
